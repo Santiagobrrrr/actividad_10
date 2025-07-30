9ef5588d37a0dd3ed2ce98f63894e8eae6af96ea
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-07-30 16:35:06 -0600

    YA REGISTRA LOS PRODUCTOS EN LISTA (función y menú)

---
+++ actividad_10.py
@@ -0,0 +1,61 @@
+products = {}
+
+def products_add(num_products):
+    for i in range(num_products):
+        print(f"\nProducto: {i + 1}")
+
+        while True:
+            code_product = input("Ingrese el código del producto: ")
+            if code_product not in products:
+                break
+            else:
+                print(f"El código '{code_product}' ya está registrado. Intente con otro.")
+
+        name_product = input(f"Ingrese el nombre del producto: ")
+        category_product = input(f"Ingrese la categoría del producto: ")
+        size_product = input(f"Ingrese el tamaño del producto: ")
+        price_product = int(input(f"Ingrese el precio unitario del producto: "))
+        stock_product = int(input(f"Ingrese el stock del producto: "))
+
+        products[code_product] = {
+            "name": name_product,
+            "category": category_product,
+            "size": size_product,
+            "price": price_product,
+            "stock": stock_product
+        }
+
+while True:
+    print(f"\nINVENTARIO DE TIENDA DE ROPA")
+    print(f"1. Añadir producto")
+    print(f"2. Inventario completo")
+    print(f"3. Buscar producto")
+    print(f"4. Precio del inventario")
+    print(f"5. Producto por categoria")
+    print(f"6. Salir")
+
+    option_user = input(f"Ingrese su opción: ")
+    match option_user:
+        case "1":
+            num_products = int(input(f"\n¿Qué cantidad de productos va a ingresar?: "))
+            products_add(num_products)
+            print(products)
+            
+        case "2":
+            print(f"\nInventario completo")
+
+        case "3":
+            print(f"\nBuscar producto")
+
+        case "4":
+            print(f"\nPrecio del inventario")
+
+        case "5":
+            print(f"\nProducto por categoria")
+
+        case "6":
+            print(f"\nSaliendo del inventario")
+            break
+
+        case _:
+            print(f"\nValor inválido, intentelo de nuevo.")
